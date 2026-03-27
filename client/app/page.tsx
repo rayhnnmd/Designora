4dbe1451600d6d6417b8ea11b7bd4278c0704035
Rayhnn <rayhnmd024@gmail.com>
2026-03-27 22:35:03 +0530

    established frontend using node.js

---
+++ client/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Welcome to Designora 🎨</h1>
+    </div>
+  );
+}
